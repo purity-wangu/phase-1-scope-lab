c0a87b639e981ffefc901d264e0599f14931e1ea
purity-wangu <purity.kariuki@student.moringaschool.com>
2022-06-15 16:47:32 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your solution in this file!
+// // Write your solution in this file!
+var customerName = "bob";
+
+function myFunction(){
+    return customerName;
+}
+
+
+function upperCaseCustomerName(){
+    var customerName = "bob";
+    window.customerName = customerName.toUpperCase();
+    
+};
+
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+
+
+function overwriteBestCustomer(){
+  bestCustomer = 'maybe bob';
+}
+
+
+
+const leastFavoriteCustomer = "Lucy";
+function changeLeastFavoriteCustomer(){
+    return  leastFavoriteCustomer = "Jack";
+}
+
